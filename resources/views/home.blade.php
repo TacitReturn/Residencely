cece579d7c5f8ceaa5485650e94a8b3f66a44d82
Glenn <Glenn G. Rudge Jr>
2022-08-22 00:02:19 -0400

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-        <div class="col-md-8">
+        <div class="col-md-8 mt-2">
             <div class="card">
                 <div class="card-header text-center">Dashboard</div>
 
