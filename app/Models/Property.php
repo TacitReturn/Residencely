1612def8a3746758f621a00b92e2b7a4bc031ea3
Glenn <Glenn G. Rudge Jr>
2022-08-30 01:05:24 -0400

    Update Property.php price and yearBuilt function

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -10,8 +10,27 @@
     {
         use HasFactory;
 
+        protected $dates = [
+            "year_built",
+        ];
+
         public function propertyManager(): BelongsTo
         {
             return $this->belongsTo(PropertyManager::class, "property_manager_id", "id", "");
+        }
+
+        /**
+         * Returns property price
+         *
+         * @return float|int|string
+         */
+        public function price(): float|int|string
+        {
+            return number_format($this->price_per_sqft * $this->sqft, 2, ".", ",");
+        }
+
+        public function yearBuilt()
+        {
+            return $this->year_built->format("Y");
         }
     }
--- resources/views/properties/show.blade.php
+++ resources/views/properties/show.blade.php
@@ -7,8 +7,22 @@
             <div class="col-md-5 mb-4 mb-md-0">
 
                 <div class="view zoom z-depth-2 rounded">
-                    <img class="img-fluid w-100" src="https://mdbootstrap.com/img/Photos/Horizontal/Eco/img(24).jpg"
-                         alt="Sample">
+                    <div class="my-5">
+                        <img class="img-fluid w-100"
+                             src="https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                             alt="Sample">
+                    </div>
+                    <a href="#!">
+                        <div class="mask"></div>
+                    </a>
+                </div>
+
+                <div class="view zoom z-depth-2 rounded mt-5">
+                    <div class="my-5">
+                        <img class="img-fluid w-100"
+                             src="https://images.pexels.com/photos/186077/pexels-photo-186077.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                             alt="Sample">
+                    </div>
                     <a href="#!">
                         <div class="mask"></div>
                     </a>
@@ -17,71 +31,85 @@
             </div>
             <div class="col-md-7">
 
-                <h5>Dark grapes from Italy, Lazio - 1kg</h5>
-                <p class="mb-2 text-muted text-uppercase small">Fruits</p>
-                <ul class="rating">
-                    <li>
-                        <i class="fas fa-star fa-sm text-primary"></i>
-                    </li>
-                    <li>
-                        <i class="fas fa-star fa-sm text-primary"></i>
-                    </li>
-                    <li>
-                        <i class="fas fa-star fa-sm text-primary"></i>
-                    </li>
-                    <li>
-                        <i class="fas fa-star fa-sm text-primary"></i>
-                    </li>
-                    <li>
-                        <i class="far fa-star fa-sm text-primary"></i>
-                    </li>
-                </ul>
-                <p><span class="mr-1"><strong>$11.99</strong></span></p>
-                <p class="pt-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, sapiente illo. Sit
-                    error voluptas repellat rerum quidem, soluta enim perferendis voluptates laboriosam. Distinctio,
-                    officia quis dolore quos sapiente tempore alias.</p>
+                <h5>{{ $property->title }}</h5>
+                <p class="mb-2 text-muted text-uppercase text-success small">
+                    {{ $property->HVAC }}
+                </p>
+                <div class="d-flex">
+                    <p>Tenant Rating</p>
+                    <ul class="rating ml-3">
+                        <li>
+                            <i class="fas fa-star fa-sm text-primary"></i>
+                        </li>
+                        <li>
+                            <i class="fas fa-star fa-sm text-primary"></i>
+                        </li>
+                        <li>
+                            <i class="fas fa-star fa-sm text-primary"></i>
+                        </li>
+                        <li>
+                            <i class="fas fa-star fa-sm text-primary"></i>
+                        </li>
+                        <li>
+                            <i class="far fa-star fa-sm text-primary"></i>
+                        </li>
+                    </ul>
+                </div>
+                <p><span class="mr-1"><strong>
+                            $ {{ $property->price() }}
+                        </strong></span></p>
+                <p class="pt-1">
+                    {{ $property->description }}
+                </p>
                 <div class="table-responsive">
                     <table class="table table-sm table-borderless mb-0">
                         <tbody>
                         <tr>
-                            <th class="pl-0 w-25" scope="row"><strong>Country</strong></th>
-                            <td>Italy</td>
+                            <th class="pl-0 w-25" scope="row"><strong>Address</strong></th>
+                            <td>{{ $property->address }}</td>
                         </tr>
                         <tr>
-                            <th class="pl-0 w-25" scope="row"><strong>Province</strong></th>
-                            <td>Lazio</td>
+                            <th class="pl-0 w-25" scope="row"><strong>Year Built</strong></th>
+                            <td>
+                                {{ $property->yearBuilt() }}
+                            </td>
                         </tr>
                         <tr>
                             <th class="pl-0 w-25" scope="row"><strong>Type</strong></th>
-                            <td>Abbuoto</td>
-                        </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <hr>
-                <div class="table-responsive mb-2">
-                    <table class="table table-sm table-borderless">
-                        <tbody>
-                        <tr>
-                            <td class="pl-0 pb-0 w-25">Quantity</td>
-                        </tr>
-                        <tr>
-                            <td class="pl-0">
-                                <div class="def-number-input number-input safari_only mb-0">
-                                    <button onclick="this.parentNode.querySelector('input[type=number]').stepDown()"
-                                            class="minus"></button>
-                                    <input class="quantity" min="0" name="quantity" value="1" type="number">
-                                    <button onclick="this.parentNode.querySelector('input[type=number]').stepUp()"
-                                            class="plus"></button>
-                                </div>
+                            <td>
+                                {{ $property->residence_type }}
                             </td>
                         </tr>
                         </tbody>
                     </table>
                 </div>
-                <button type="button" class="btn btn-primary btn-md mr-1 mb-2">Buy now</button>
+                <hr>
+                {{--                <div class="table-responsive mb-2">--}}
+                {{--                    <table class="table table-sm table-borderless">--}}
+                {{--                        <tbody>--}}
+                {{--                        <tr>--}}
+                {{--                            <td class="pl-0 pb-0 w-25">Quantity</td>--}}
+                {{--                        </tr>--}}
+                {{--                        <tr>--}}
+                {{--                            <td class="pl-0">--}}
+                {{--                                <div class="def-number-input number-input safari_only mb-0">--}}
+                {{--                                    <button onclick="this.parentNode.querySelector('input[type=number]').stepDown()"--}}
+                {{--                                            class="minus"></button>--}}
+                {{--                                    <input class="quantity" min="0" name="quantity" value="1" type="number">--}}
+                {{--                                    <button onclick="this.parentNode.querySelector('input[type=number]').stepUp()"--}}
+                {{--                                            class="plus"></button>--}}
+                {{--                                </div>--}}
+                {{--                            </td>--}}
+                {{--                        </tr>--}}
+                {{--                        </tbody>--}}
+                {{--                    </table>--}}
+                {{--                </div>--}}
+                <button type="button" class="btn btn-primary btn-md mr-1 mb-2">
+                    Update Property
+                </button>
                 <button type="button" class="btn btn-light btn-md mr-1 mb-2"><i
-                            class="fas fa-shopping-cart pr-2"></i>Add to cart
+                            class="fas fa-shopping-cart pr-2"></i>
+                    View Documents
                 </button>
             </div>
         </div>
